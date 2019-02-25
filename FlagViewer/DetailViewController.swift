f15be74f1bc2e9428b13a0fe5467eec3d2984fba
Michele Galvagno <michicello@gmail.com>
2019-02-25 16:49:36 +0100

    Challenge finished
    Added:
    -possibility to share
    - String extension for fileName without file extension
    - imageView to the cell

--- FlagViewer/DetailViewController.swift
+++ FlagViewer/DetailViewController.swift
@@ -13,15 +13,39 @@ class DetailViewController: UIViewController {
     var selectedImage: String?
     
     // MARK: - Outlets & Actions
-    @IBOutlet var image: UIImageView!
+    @IBOutlet var imageView: UIImageView!
     
-    // MARK: - Navigation
-
+    // MARK: - Navigation & Views
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+        
     // MARK: - Methods
-    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let imageName = selectedImage?.fileName().uppercased()
+        let activityViewController = UIActivityViewController(activityItems: [image, "Whose country is this flag?It's ISO country code is \(imageName!)"], applicationActivities: [])
+        activityViewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(activityViewController, animated: true)
+    }
 }
